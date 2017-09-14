f40470d679ef2d428999eb72a1ca6794c3d02c54
Lee <Lee@192.168.32.1>
2017-09-14 21:54:19 +0800

    111

---
+++ taotao-portal/src/main/java/com/taotao/portal/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.taotao.portal.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.taotao.portal.pojo.CartItem;
+import com.taotao.portal.service.CartService;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+	@Autowired
+	private CartService cartService;
+	@RequestMapping("/order-cart")
+	public String showOrderCart(HttpServletRequest request,HttpServletResponse response,Model model){
+		List<CartItem> list = cartService.getCartItemList(request, response);
+		model.addAttribute("cartList", list);
+		return "order-cart";
+	}
+}
