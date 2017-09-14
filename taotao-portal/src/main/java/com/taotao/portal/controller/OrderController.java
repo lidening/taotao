9b9802c867ab8e57f1a41524d8fe1e1b0f20c65c
Lee <Lee@192.168.32.1>
2017-09-14 23:35:26 +0800

    111

--- taotao-portal/src/main/java/com/taotao/portal/controller/OrderController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/OrderController.java
@@ -5,13 +5,16 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.taotao.portal.pojo.CartItem;
+import com.taotao.portal.pojo.Order;
 import com.taotao.portal.service.CartService;
+import com.taotao.portal.service.OrderService;
 
 @Controller
 @RequestMapping("/order")
@@ -19,10 +22,27 @@ public class OrderController {
 
 	@Autowired
 	private CartService cartService;
+	@Autowired
+	private OrderService orderService;
 	@RequestMapping("/order-cart")
 	public String showOrderCart(HttpServletRequest request,HttpServletResponse response,Model model){
 		List<CartItem> list = cartService.getCartItemList(request, response);
 		model.addAttribute("cartList", list);
 		return "order-cart";
+	}
+	@RequestMapping("/create")
+	public String createOrder(Order order,Model model){
+		try {
+			String orderId = orderService.createOrder(order);
+			model.addAttribute("orderId",orderId);
+			model.addAttribute("payment", order.getPayment());
+			model.addAttribute("date", new DateTime().plusDays(3).toString("YYYY-MM-dd"));
+			return "success";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("message", "订单失败，请稍后重试！");
+			return "error/exception";
+		}
 	}
 }
---
+++ taotao-portal/src/main/java/com/taotao/portal/service/impl/OrderServiceImpl.java
@@ -0,0 +1,36 @@
+package com.taotao.portal.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.HttpClientUtil;
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.portal.pojo.Order;
+import com.taotao.portal.service.OrderService;
+@Service
+public class OrderServiceImpl implements OrderService {
+
+	@Autowired
+	private HttpClientUtil httpClientUtil;
+	
+	@Value("${ORDER_BASE_URL}")
+	private String ORDER_BASE_URL;
+	
+	@Value("${ORDER_CREATE_URL}")
+	private String ORDER_CREATE_URL;
+	
+	@Override
+	public String createOrder(Order order) {
+		//获取订单号
+		String json = HttpClientUtil.doPostJson(ORDER_BASE_URL+ORDER_CREATE_URL, JsonUtils.objectToJson(order));
+		TaotaoResult taotaoResult = TaotaoResult.format(json);
+		if(taotaoResult.getStatus()==200){
+			Object orderId= (Long)taotaoResult.getData();
+			return orderId.toString();
+		}
+		return "";
+	}
+
+}
